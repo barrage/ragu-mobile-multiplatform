83cb5c0a9b0361d95dd694fa3339708cdce2e1f3
Leo Svjetličić <leo.svjetlicic@barrage.net>
2024-11-08 12:35:54 +0100

    Fix current user card text color

--- composeApp/src/commonMain/kotlin/net/barrage/chatwhitelabel/ui/screens/history/components/currentuser/CurrentUserCard.kt
+++ composeApp/src/commonMain/kotlin/net/barrage/chatwhitelabel/ui/screens/history/components/currentuser/CurrentUserCard.kt
@@ -1,22 +1,22 @@
 package net.barrage.chatwhitelabel.ui.screens.history.components.currentuser
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.dp
@@ -30,8 +30,8 @@ fun CurrentUserCard(
     modifier: Modifier = Modifier,
     onUserClick: () -> Unit,
 ) {
-    Card(modifier.padding(8.dp).clip(RoundedCornerShape(12.dp)).clickable { onUserClick() }) {
-        Box(modifier = Modifier.padding(8.dp)) {
+    Card(modifier.padding(8.dp)) {
+        Box(modifier = Modifier) {
             when (viewState) {
                 HistoryScreenStates.Error -> {
                     Text(text = "Error loading data.", color = Red, fontSize = 24.sp)
@@ -43,12 +43,27 @@ fun CurrentUserCard(
                 }
 
                 is HistoryScreenStates.Success<CurrentUser> -> {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(imageVector = Icons.Filled.Person, contentDescription = "user profile")
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Column(modifier = Modifier.weight(1f)) {
-                            Text(text = viewState.data.fullName, fontSize = 20.sp)
-                            Text(text = viewState.data.email, fontSize = 12.sp, overflow = Ellipsis)
+                    TextButton(onClick = onUserClick) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                imageVector = Icons.Filled.Person,
+                                contentDescription = "user profile",
+                                tint = Gray,
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Column(modifier = Modifier.weight(1f)) {
+                                Text(
+                                    text = viewState.data.fullName,
+                                    style = MaterialTheme.typography.labelLarge,
+                                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                                )
+                                Text(
+                                    text = viewState.data.email,
+                                    style = MaterialTheme.typography.bodySmall,
+                                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                                    overflow = Ellipsis,
+                                )
+                            }
                         }
                     }
                 }
